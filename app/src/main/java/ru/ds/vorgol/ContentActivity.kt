c815d1e36e5c85d4db90834acf683de4ed7fe2f1
DmitrySergienko <dmitrisergienko@gmail.com>
2022-04-12 18:03:14 +0300

    PhotoView for zoom

--- app/src/main/java/ru/ds/vorgol/ContentActivity.kt
+++ app/src/main/java/ru/ds/vorgol/ContentActivity.kt
@@ -1,23 +1,29 @@
 package ru.ds.vorgol
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import ru.ds.vorgol.databinding.ActivityContentBinding
-import ru.ds.vorgol.databinding.ContentFragmentBinding
-import ru.ds.vorgol.databinding.MainFragmentBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.github.chrisbanes.photoview.PhotoView
+import ru.ds.vorgol.databinding.SectorItemBinding
+
 
 class ContentActivity : AppCompatActivity() {
-    lateinit var binding: ContentFragmentBinding
+    lateinit var binding: SectorItemBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ContentFragmentBinding.inflate(layoutInflater)
+        binding = SectorItemBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//zoom PhotoView
+        val photoView = binding.imageView as PhotoView
+        photoView.setImageResource(intent.getIntExtra("image",R.drawable.ic_place_wiki))
 
-        with(binding){
-            textViewTitle.text = intent.getStringExtra("Title")
-            textViewContext.text = intent.getStringExtra("context")
-            imageView.setImageResource(intent.getIntExtra("image",R.drawable.bg_earth))
-        }
+       with(binding){
+           textViewTitle.text = intent.getStringExtra("Title")
+           textViewContent.text = intent.getStringExtra("context")
+           imageView.setImageResource(intent.getIntExtra("image",R.drawable.bg_earth))
+       }
+
+
 
     }
 }
